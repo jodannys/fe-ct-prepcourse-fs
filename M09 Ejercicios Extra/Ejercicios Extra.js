707a935e2fc510ccc075018315f237d1efced680
¨jodannys¨ <jodannysaliendres@gmail.com>
2023-08-16 02:35:16 -0500

    mi primer commit

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,15 +6,37 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   const arregloVacio = [];
+   for (const [clave, valor] of Object.entries(objeto)) {
+      arregloVacio.push([clave, valor])
+   }
+   return arregloVacio;
 }
+
 
 function numberOfCharacters(string) {
    // La función recibe un string. Debes recorrerlo y retornar un objeto donde cada propiedad es una de las
    // letras del string, y su valor es la cantidad de veces que se repite en el string.
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
-   // Tu código:
+   // Tu código:function numberOfCharacters(string) {
+    const charCount = {};
+    
+    for (let char of string) {
+        if (charCount[char]) {
+            charCount[char]++;
+        } else {
+            charCount[char] = 1;
+        }
+    }
+
+    const sortedCharCount = {};
+    Object.keys(charCount).sort().forEach(key => {
+        sortedCharCount[key] = charCount[key];
+    });
+    return sortedCharCount;
 }
+
 
 function capToFront(string) {
    // Recibes un string con algunas letras en mayúscula y otras en minúscula.
@@ -22,25 +44,48 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
-}
+      const uppercaseLetters = string.split('').filter(char => char === char.toUpperCase());
+      const lowercaseLetters = string.split('').filter(char => char === char.toLowerCase());
+      return uppercaseLetters.join('') + lowercaseLetters.join('');
+  }
+
 
 function asAmirror(frase) {
    // Recibes una frase. Tu tarea es retornar un nuevo string en el que el orden de las palabras sea el mismo.
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
-}
+      const palabras = frase.split(' ');
+      const palabrasInvertidas = palabras.map(palabra => palabra.split('').reverse().join(''));
+      return palabrasInvertidas.join(' ');
+  }
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
    // Tu código:
+   const numeroStr = numero.toString();
+   const longitud = numeroStr.length;
+
+   for (let i = 0; i < longitud / 2; i++) {
+      if (numeroStr[i] !== numeroStr[longitud - 1 - i]) {
+         return "No es capicua";
+      }
+   }
+   return "Es capicua";
 }
+
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
    // Tu código:
+   var nuevoString = "";
+   for (var i = 0; i < string.length; i++)
+      if (string[i] !== "a" && string[i] !== "b" && string[i] !== "c") {
+         nuevoString += string[i]
+      }
+   return nuevoString
 }
 
 function sortArray(arrayOfStrings) {
@@ -49,6 +94,7 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   return arrayOfStrings.sort((a, b) => a.length - b.length);
 }
 
 function buscoInterseccion(array1, array2) {
@@ -58,6 +104,13 @@ function buscoInterseccion(array1, array2) {
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
    // Tu código:
+   var nuevoArray = [];
+   for (const num of array1) {
+      if (array2.includes(num)) {
+         nuevoArray.push(num)
+      }
+   }
+   return nuevoArray;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
